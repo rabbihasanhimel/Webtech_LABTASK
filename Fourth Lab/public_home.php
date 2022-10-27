58d9e4eb3372ab74f4d3ca08e8e2ba67e33108ea
rabbihasanhimel <96992475+rabbihasanhimel@users.noreply.github.com>
2022-10-27 12:02:44 +0600

    Add files via upload

---
+++ Fourth Lab/public_home.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>Home</title>
+	<style>
+		.color_C{
+			color: #C70039;
+			font-family: Comic;
+		}
+	</style>
+</head>
+<body>
+	<?php include 'top.php';?>
+	<fieldset>
+		<h1 class = "color_C">Welcome to Messi's World</h1>
+	</fieldset>
+	<?php include 'footer.php';?>
+	
+</body>
+</html>
---
+++ Fourth Lab/top.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<title>Header</title>
+</head>
+<style>
+body {
+  background-image:  url('5962476.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;  
+  background-size: cover;
+}
+</style>
+<body>	
+	
+	<fieldset>
+		<div class="header">
+      <a href="https://ibb.co/sRkmtb8"><img src="https://i.ibb.co/sRkmtb8/R.jpg" alt="R" border="0"></a>
+		<br><br><div class="navigation">
+				<div class="home"><a href="public_home.php">Home<span> |</span></a></div>
+				<div class="login"><a href="login.php">Log in<span> |</span></a></div>
+				<div class="registration"><a href="registration.php">Registration</a></div>
+			</div>
+		</div>
+	</fieldset>
+</body>
+</html>
