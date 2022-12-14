36732bb00290ff7fb4d10692117e1fb35c26a7b0
rabbihasanhimel <96992475+rabbihasanhimel@users.noreply.github.com>
2022-12-15 00:29:43 +0600

    Delete fifth lab/lab_5(editProducts) directory

--- fifth lab/lab_5(editProducts)/nav.php
+++
@@ -1,5 +0,0 @@
-<ul>
-        <li><a href="addProduct.php"> Add product</a></li>
-        <li><a href="showAllProduct.php"> Show all product</a></li>
-        <li><a href="ajax/index.php"> Search product</a></li>
-    </ul>
